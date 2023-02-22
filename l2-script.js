8ed59ca75a0a0b0166cc78950e5af40d8823241e
Alexia Hall <alexiahall306@icloud.com>
2023-02-22 09:53:20 -0600

    fe

--- l2-script.js
+++ l2-script.js
@@ -1,13 +1,9 @@
 let nickName = prompt("My nick name is Myrrh. true or false?");
-console.log(nickName);
+
 let threejobs = prompt("I worked 3 or more jobs. true or false?");
-console.log(threejobs);
+
 let hrs = prompt("I worked 12 hours. true or false?");
-console.log(hrs);
+
 let rugby = prompt("I played rugby in highschool. true or false?");
-console.log(rugby);
+
 let mca = prompt("First year of college was shut down. true or false?");
-console.log(mca);
-
-}
-
