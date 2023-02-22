82e2327906f027fdaafd24e56c57620217097487
Alexia Hall <alexiahall306@icloud.com>
2023-02-22 13:49:17 -0600

    gdf

--- l2-script.js
+++ l2-script.js
@@ -1,9 +1,61 @@
-let nickName = prompt("My nick name is Myrrh. true or false?");
+function quizAboutme() {
+  let nickName = prompt("My nick name is Myrrh. true or false?");
+  nickName = nickName.toLowerCase().toUpperCase().trim();
+  if (nickName == "false") {
+    alert("correct!");
+    console.log(
+      "when answering true or false if you get it right message will show correct"
+    );
+  } else {
+    alert("wrong");
+    console.log("la de da de da you're wrong");
+  }
 
-let threejobs = prompt("I worked 3 or more jobs. true or false?");
+  let threeJobs = prompt("I worked 3 or more jobs. true or false?");
+  threeJobs = threeJobs.toLowerCase().toUpperCase().trim();
+  if (threeJobs == "true") {
+    alert("correct!");
+    console.log(
+      "when answering true or false if you get it right message will show correct"
+    );
+  } else {
+    alert("wrong");
+    console.log("la de da de da you're wrong");
+  }
 
-let hrs = prompt("I worked 12 hours. true or false?");
+  let hrs = prompt("I worked 12 hours. true or false?");
+  hrs = hrs.toLowerCase().toUpperCase().trim();
+  if (threeJobs == "true") {
+    alert("correct!");
+    console.log(
+      "when answering true or false if you get it right message will show correct"
+    );
+  } else {
+    alert("wrong");
+    console.log("la de da de da you're wrong");
+  }
 
-let rugby = prompt("I played rugby in highschool. true or false?");
+  let rugby = prompt("I played rugby in highschool. true or false?");
+  rugby = rugby.toLowerCase().toUpperCase().trim();
+  if (threeJobs == "true") {
+    alert("correct!");
+    console.log(
+      "when answering true or false if you get it right message will show correct"
+    );
+  } else {
+    alert("wrong");
+    console.log("la de da de da you're wrong");
+  }
 
-let mca = prompt("First year of college was shut down. true or false?");
+  let mca = prompt("First year of college was shut down. true or false?");
+  mca = mca.toLowerCase().toUpperCase().trim();
+  if (mca == "true") {
+    alert("correct!");
+    console.log(
+      "when answering true or false if you get it right message will show correct"
+    );
+  } else {
+    alert("wrong");
+    console.log("la de da de da you're wrong");
+  }
+}
